4c99d21986880e49b1f3fbe8b1936ef7fde2d351
Heri Setiawan <heri.setiawan@tech.jago.com>
2025-08-07 10:06:54 +0700

    chore: update nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,41 +1,26 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = '\\'
-
-for k, v in pairs({
-  number = true,
-  wrap = false,
-  cmdheight = 0,
-  tabstop = 2,
-  shiftwidth = 2,
-  softtabstop = 2,
-  expandtab = true,
-  foldmethod = 'indent',
-  foldexpr = 'nvim_treesitter#foldexpr()',
-  foldlevelstart = 1,
-  statusline = '[%{v:lua.M()}] %f %r%m %=%{&ft} %{v:lua.L()}',
-  mouse = '',
-}) do vim.o[k] = v end
-
-function _G.M()
-  return ({ n = 'N', i = 'I', v = 'V', V = 'VL', ['\026'] = 'VB', c = 'C', R = 'R', t = 'T' })
-      [vim.api.nvim_get_mode().mode] or '?'
-end
-
-function _G.L()
-  local n = vim.tbl_map(function(c) return c.name end, vim.lsp.get_clients({ bufnr = 0 }))
-  return #n > 0 and '(' .. table.concat(n, ', ') .. ')' or ''
-end
-
+vim.o.number = true
+vim.o.wrap = false
+vim.o.cmdheight = 0
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+vim.o.softtabstop = 2
+vim.o.expandtab = true
+vim.o.foldmethod = 'indent'
+vim.o.foldexpr = 'nvim_treesitter#foldexpr()'
+vim.o.foldlevelstart = 1
+vim.o.statusline = '[%{v:lua.M()}] %f %r%m %=%{&ft} %{v:lua.L()}'
+vim.o.mouse = ''
 vim.diagnostic.config({ virtual_lines = { current_line = true } })
-
-vim.pack.add(vim.tbl_map(function(u) return u:match('^https://') and u or 'https://github.com/' .. u end,
-  { 'nvim-lua/plenary.nvim', 'ibhagwan/fzf-lua', 'f-person/git-blame.nvim', 'hrsh7th/cmp-buffer', 'hrsh7th/cmp-nvim-lsp',
-    'hrsh7th/cmp-path', 'hrsh7th/nvim-cmp', 'nvim-flutter/flutter-tools.nvim', 'nvim-treesitter/nvim-treesitter',
-    'catppuccin/nvim', 'mistweaverco/kulala.nvim', 'MeanderingProgrammer/render-markdown.nvim', 'nvim-tree/nvim-tree.lua' }))
-
 vim.lsp.enable({ 'eslint_ls', 'kotlin_ls', 'lua_ls', 'tailwindcss_ls', 'ts_ls', 'rust_ls', 'kulala.ls' })
-
-local setup = {
+vim.pack.add(vim.tbl_map(function(u) return u:match('^https://') and u or 'https://github.com/' .. u end, {
+  'nvim-lua/plenary.nvim', 'ibhagwan/fzf-lua', 'f-person/git-blame.nvim', 'hrsh7th/cmp-buffer',
+  'hrsh7th/cmp-nvim-lsp', 'hrsh7th/cmp-path', 'hrsh7th/nvim-cmp', 'nvim-flutter/flutter-tools.nvim',
+  'nvim-treesitter/nvim-treesitter', 'catppuccin/nvim', 'mistweaverco/kulala.nvim',
+  'MeanderingProgrammer/render-markdown.nvim', 'nvim-tree/nvim-tree.lua'
+}))
+for k, v in pairs({
   cmp = function(cmp)
     cmp.setup({
       snippet = { expand = function(a) vim.snippet.expand(a.body) end },
@@ -51,15 +36,13 @@ local setup = {
     })
   end,
   catppuccin = function(catppuccin)
-    catppuccin.setup { transparent_background = true }
+    catppuccin.setup({ transparent_background = true, integrations = { fzf = true, } })
     vim.cmd.colorscheme 'catppuccin'
   end,
   ['nvim-tree'] = function(tree) tree.setup({ update_focused_file = { enable = true } }) end
-}
+}) do v(require(k)) end
 
-for k, v in pairs(setup) do v(require(k)) end
-
-local keymaps = {
+for m, s in pairs({
   x = {
     Y = '"+y',
     ['<leader>B'] = '<cmd>let @+=expand(\'%:p\')<cr>',
@@ -78,6 +61,14 @@ local keymaps = {
     ['<leader>lr'] = vim.lsp.buf.rename,
     ['<leader>lf'] = vim.lsp.buf.format,
   }
-}
+}) do for k, c in pairs(s) do vim.keymap.set(m, k, c) end end
 
-for m, s in pairs(keymaps) do for k, c in pairs(s) do vim.keymap.set(m, k, c) end end
+function _G.M()
+  return ({ n = 'N', i = 'I', v = 'V', V = 'VL', ['\026'] = 'VB', c = 'C', R = 'R', t = 'T' })
+      [vim.api.nvim_get_mode().mode] or '?'
+end
+
+function _G.L()
+  local n = vim.tbl_map(function(c) return c.name end, vim.lsp.get_clients({ bufnr = 0 }))
+  return #n > 0 and '(' .. table.concat(n, ', ') .. ')' or ''
+end
