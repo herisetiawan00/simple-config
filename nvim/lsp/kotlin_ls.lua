d824cde865755dd6cc2defafcb0bb2cbfae0387c
Heri Setiawan <hr.bltz1@gmail.com>
2026-04-06 20:30:05 +0700

    chore: initial commit

---
+++ nvim/lsp/kotlin_ls.lua
@@ -0,0 +1,13 @@
+return {
+	filetypes = { 'kotlin' },
+	cmd = { 'kotlin-language-server' },
+	-- cmd = { 'kotlin-lsp', '--stdio' },
+	root_markers = {
+		'settings.gradle', -- Gradle (multi-project)
+		'settings.gradle.kts', -- Gradle (multi-project)
+		'pom.xml', -- Maven
+		'build.gradle', -- Gradle
+		'build.gradle.kts', -- Gradle
+		'workspace.json', -- Used to integrate your own build system
+	},
+}
